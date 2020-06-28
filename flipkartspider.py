b6176966a0915a0f25aae84e92848386fc0f4049
rajnan991 <67545412+rajnan991@users.noreply.github.com>
2020-06-29 03:49:59 +0530

    Add files via upload

---
+++ flipkartspider.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from ..items import flipkartItem
+
+class FlipkartspiderSpider(scrapy.Spider):
+    name = 'flipkartspider'
+    page_number = 2
+    start_urls = ['https://www.flipkart.com/mobiles/mi~brand/pr?sid=tyy,4io&otracker=nmenu_sub_Electronics_0_Mi']
+
+    def parse(self ,response ):
+        items = flipkartItem()
+
+        product_name = response.css('._3wU53n').extract()
+        product_price = response.css('._2rQ-NK').css('::text').extract()
+        product_rating = response.css('.hGSR34').css('::text').extract()
+        product_storage = response.css('.tVe95H:nth-child(1)').css('::text').extract()
+        product_display = response.css('.tVe95H:nth-child(2)').css('::text').extract()
+        product_camera = response.css('.tVe95H:nth-child(3)').css('::text').extract()
+        product_battery = response.css('.tVe95H:nth-child(4)').css('::text').extract()
+        
+        
+
+        items['product_name'] = product_name
+        items['product_price'] = product_price
+        items['product_rating'] = product_rating
+        items['product_storage'] = product_storage
+        items['product_display'] = product_display
+        items['product_camera'] = product_camera
+        items['product_battery'] = product_battery
+
+        yield items
+
+    next_page = 'https://www.flipkart.com/mobiles/mi~brand/pr?sid=tyy%2C4io&otracker=nmenu_sub_Electronics_0_Mi&page=' + str(FlipkartspiderSpider.page_number)
+        if FlipkartspiderSpider.page_number <= 10:
+            FlipkartspiderSpider.page_number = 1
+            yield response.follow(next_page, callback =self.parse)
+         
+   
---
+++ items.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class flipkartItem(scrapy.Item):
+    # define the fields for your item here like:
+    product_name = scrapy.Field()
+    product_price = scrapy.Field()
+    product_rating = scrapy.Field()
+    product_storage = scrapy.Field()
+    product_display = scrapy.Field()
+    product_camera = scrapy.Field()
+    product_battery = scrapy.Field()
+    
+    
+    pass
